ddc988923ac3b62fa0c2bb3748122277b8857f5b
Vincent Chin <vince.chin.aie@gmail.com>
2017-04-22 14:39:19 +1000

    added scaleInterval to squash function

--- Assets/LudumDare38/Scripts/Volcano.cs
+++ Assets/LudumDare38/Scripts/Volcano.cs
@@ -9,12 +9,16 @@ public class Volcano : BaseDisaster
     public float m_eruptInterval = 2.0f;
     public float m_eruptionForce = 100.0f;
     public float m_eruptionPointOffset = 0.5f;
+    public float m_scaleIntervalDenominator = 10.0f;
     public uint m_numOfRocks = 1;
     private Vector3 eruptPosition;
+    private float m_scaleInterval = 0.0f;
+
     // Use this for initialization
     void Start()
     {
-        
+
+        m_scaleInterval = this.transform.localScale.y / m_scaleIntervalDenominator;
         //get peak of collider;
         eruptPosition = this.transform.position + this.transform.up * m_eruptionPointOffset;
     }
@@ -27,7 +31,7 @@ public class Volcano : BaseDisaster
 
         if(m_timer >= m_eruptInterval)
         {
-            Squash(1);
+            Squash(m_scaleInterval);
             Erupt();
             m_timer = 0.0f;
         }
@@ -65,7 +69,6 @@ public class Volcano : BaseDisaster
     private void Squash(float a_sqaushAmount)
     {
         this.transform.localScale -= this.transform.up * a_sqaushAmount;
-        this.transform.localPosition -= this.transform.up * Time.deltaTime;
     }
 
     private void SelfDestruct()
