575da4a05e60c47b48cf6d9385f0600cca75e939
James Owen <jamesowen012@gmail.com>
2017-04-22 21:18:45 +1000

    added a rotate script to Planet and StarField, removed color tints from planet and volcano changed meteor to a new layer that doesnt hit disasters

---
+++ Assets/LudumDare38/Scripts/RotateObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+
+	public float m_Speed;
+	public Vector3 m_Axis = Vector3.forward;
+
+	public bool m_RandomStartRotation = true;
+
+	void Start() {
+		if (m_RandomStartRotation) {
+			transform.Rotate(m_Axis * Random.Range(0.0f, 360.0f));
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(m_Axis * m_Speed * Time.deltaTime);
+	}
+}
