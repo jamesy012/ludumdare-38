d0a44ac0c77c40afcc433fae1dc40b96003ef277
James Owen <jamesowen012@gmail.com>
2017-04-24 09:53:46 +1000

    If sound is muted then no audio will play

---
+++ Assets/LudumDare38/Scripts/DestroyAudioIfMute.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAudioIfMute : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (GameManager.m_Muted) {
+			Destroy(GetComponent<AudioSource>());
+		}
+		Destroy(this);
+	}
+}
