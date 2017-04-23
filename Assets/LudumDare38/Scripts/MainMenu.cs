176c6c94b0b3e580910fc989e1824c3706c3548b
Vincent Chin <vince.chin.aie@gmail.com>
2017-04-24 08:51:06 +1000

    added main menu scene and script

---
+++ Assets/LudumDare38/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Endless()
+    {
+
+    }
+
+    public void Credits()
+    {
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
