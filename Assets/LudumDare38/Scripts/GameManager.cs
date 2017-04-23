e45bf985a60bd27d9df93cf7a7771eb365916b74
Vincent Chin <vince.chin.aie@gmail.com>
2017-04-23 16:13:19 +1000

    added game manager script

---
+++ Assets/LudumDare38/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int m_inhabitantCount = 0;
+    private GameObject[] m_inhabitants;
+    // Use this for initialization
+    void Start()
+    {
+
+      m_inhabitants =  GameObject.FindGameObjectsWithTag("Inhabitant");
+      m_inhabitantCount = m_inhabitants.Length;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_inhabitants = GameObject.FindGameObjectsWithTag("Inhabitant");
+        m_inhabitantCount = m_inhabitants.Length;
+
+        if(m_inhabitantCount <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+
+    }
+}
