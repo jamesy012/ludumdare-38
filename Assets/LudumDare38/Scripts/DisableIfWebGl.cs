e374535a936f80e6255afdf27402227e18106f72
James Owen <jamesowen012@gmail.com>
2017-04-24 18:49:56 +1000

    Removed Exit button from main menu only in webGl builds

---
+++ Assets/LudumDare38/Scripts/DisableIfWebGl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableIfWebGl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+#if UNITY_WEBGL
+		gameObject.SetActive(false);		
+#endif
+	}
+}
